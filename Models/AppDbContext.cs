4110583367bc99611bd16e59c6fcb4ad7dbeed96
daniel Bran <recuerdosdiarios4@gmail.com>
2025-03-20 19:24:14 -0600

    se Agrego appDbContext y la clas user que es la tabla principal

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Gestor_de_tareas.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Gestor_de_tareas.Models
+{
+    public class User
+    {
+        public int Id_user{ get; set; }
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
